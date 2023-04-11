85cc1809a22fb569c6e07ccb37275133da08d3c6
Miguel Aldo Balderrama Vaca <miguel.aldobv@gmail.com>
2023-04-10 20:52:55 -0400

    added get, post, put , delete a board

--- trello-api-autoamtion/src/test/java/com/qaacademy/module4/automation/trello/api/steps/BoardsSteps.java
+++ trello-api-autoamtion/src/test/java/com/qaacademy/module4/automation/trello/api/steps/BoardsSteps.java
@@ -6,6 +6,7 @@ import com.qaacademy.module4.automation.core.api.client.RequestManager;
 import com.qaacademy.module4.automation.core.api.environment.EnvironmentManager;
 import com.qaacademy.module4.automation.trello.api.validator.RequestManagerValidator;
 import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.jupiter.api.Assertions;
@@ -13,21 +14,26 @@ import org.junit.jupiter.api.Assertions;
 import java.util.HashMap;
 
 public class BoardsSteps {
-
+    private final HashMap<String, String> context;
     private final EnvironmentManager environmentManager;
     private final HashMap<String, String> params;
     private final HashMap<String, String> headers;
     private final ApiRequest apiRequest;
     private ApiResponse response;
 
-    public BoardsSteps() {
+    public BoardsSteps(final HashMap<String, String> context) {
+        this.context = context;
         environmentManager = EnvironmentManager.getInstance();
         params = new HashMap<>();
-        params.put("key", environmentManager.getKey());
-        params.put("token", environmentManager.getToken());
         headers = new HashMap<>();
         headers.put("Content-Type", "application/json");
         apiRequest = new ApiRequest();
+    }
+
+    @Given("I have a valid key and token")
+    public void haveAValidKeyAndToken() {
+        params.put("key", environmentManager.getKey());
+        params.put("token", environmentManager.getToken());
     }
 
     @When("I send a request to GET boards endpoint {string}")
@@ -35,6 +41,55 @@ public class BoardsSteps {
         var url = environmentManager.getUrl().concat(endpoint);
         apiRequest.setParams(params);
         response = RequestManager.get(apiRequest, url);
+    }
+
+    @When("I send a request to GET a board endpoint {string}")
+    public void sendARequestToGETABoardEndpoint(String endpoint) {
+        var id = context.get("boardId");
+        var url = environmentManager.getUrl().concat(endpoint.replace("{id}", id));
+        apiRequest.setParams(params);
+        response = RequestManager.get(apiRequest, url);
+    }
+
+    @When("I send a request to POST boards endpoint {string} with name {string}")
+    public void sendARequestToPOSTBoardsEndpointWithName(String endpoint, String name) {
+        var url = environmentManager.getUrl().concat(endpoint);
+        apiRequest.setParams(params);
+        apiRequest.setHeaders(headers);
+        var bodyRequest = """
+                {
+                    "name": "%s"
+                }
+                """;
+        apiRequest.setBody(String.format(bodyRequest, name));
+        response = RequestManager.post(apiRequest, url);
+        var id = RequestManagerValidator.getId(response.getBody());
+        context.put("boardName", name);
+        context.put("boardId", id);
+    }
+
+    @When("I send a request to PUT boards endpoint {string} with name {string}")
+    public void sendARequestToPUTBoardsEndpointWithName(String endpoint, String name) {
+        var id = context.get("boardId");
+        var url = environmentManager.getUrl().concat(endpoint.replace("{id}", id));
+        apiRequest.setParams(params);
+        apiRequest.setHeaders(headers);
+        var bodyRequest = """
+                {
+                    "name": "%s"
+                }
+                """;
+        apiRequest.setBody(String.format(bodyRequest, name));
+        response = RequestManager.put(apiRequest, url);
+        context.put("boardName", name);
+    }
+
+    @When("I send a request to DELETE boards endpoint {string}")
+    public void sendARequestToDELETEBoardsEndpoint(String endpoint) {
+        var id = context.get("boardId");
+        var url = environmentManager.getUrl().concat(endpoint.replace("{id}", id));
+        apiRequest.setParams(params);
+        response = RequestManager.delete(apiRequest, url);
     }
 
     @Then("the response status code should be {int}")
@@ -49,6 +104,8 @@ public class BoardsSteps {
 
     @And("the response should have a field {string} with value {string}")
     public void theResponseShouldHaveAFieldWithValue(String field, String value) {
+        var boardName = context.get("boardName");
+        value = value.replace("{name}", boardName);
         Assertions.assertEquals(value, RequestManagerValidator.getBoardName(response.getBody(), field, value));
     }
 }
--- trello-api-autoamtion/src/test/java/com/qaacademy/module4/automation/trello/api/validator/RequestManagerValidator.java
+++ trello-api-autoamtion/src/test/java/com/qaacademy/module4/automation/trello/api/validator/RequestManagerValidator.java
@@ -9,4 +9,8 @@ public final class RequestManagerValidator {
     public static String getBoardName(String json, String field, String value) {
         return JsonPath.getResultList(json, String.format("$[?(@.name == '%s')].".concat(field), value)).get(0).toString();
     }
+
+    public static String getId(String json) {
+        return JsonPath.getResult(json, "$.id").toString();
+    }
 }
